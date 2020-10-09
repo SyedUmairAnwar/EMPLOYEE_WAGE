1896916374ce6dfc7b9e709cf4f2b6ebd163210c
syed <umaranwar707@gmail.com>
2020-10-09 10:30:07 +0530

    Modified:funtion on wages

--- emp.sh
+++ emp.sh
@@ -1,24 +1,32 @@
 #!/bin/bash -x
-MAXHRS=40
-IS_FULL_TIME=1
-IS_PART_TIME=2
-NUM_OF_WORKING_DAYS=20
+isfulltime=1
+isparttime=2
+emprateperhour=20
+noofworkingdays=20
+maxhoursinmonth=20
 totalemphrs=0
-RATE=20
-totaldays=0
-while [ $MAXHRS -gt $totalemphrs -a $totaldays -lt $NUM_OF_WORKING_DAYS ]
+totalworkingdays=0
+
+function getworkinghours(){
+ case $1 in
+        $isfulltime) emphrs=8;;
+        $isparttime) emphrs=4;;
+        *) emphrs=0;;
+ esac
+echo $emphrs
+}
+function getempwage(){
+        echo $(($1*$emprateperhour))
+}
+
+
+while [[ $totalemphrs -lt $maxhoursinmonth && $totalworkingdays -lt $noofworkingdays ]]
 do
-        ((totalday++))
-        random=$((RANDOM%3))
-        echo $random
-        case $random in
-                $IS_FULLTIME) hrs=8;;
-                $IS_PART_TIME) hrs=4;;
-                 *) hrs=0;;
-        esac
-        totalemphrs=$(($totalemphrs+$hrs))
+        ((totalworkingdays++))
+        emphrs=$( getworkinghours $((RANDOM%3)) )
+        totalemphrs=$(($emphrs + $totalemphrs))
+        dailywages[$totalworkingdays]=$( getempwage $emphrs )
 done
-
-totalSalary=$(($RATE+$totalemphrs))
-echo "total salary is:" $totalSalary
-
+totalsalary=$(($emprateperhour*$totalemphrs))
+echo "total salary is $totalsalary"
+echo ${dailywages[@]}
