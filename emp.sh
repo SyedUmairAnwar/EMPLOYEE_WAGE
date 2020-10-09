fcb693a6acc184a73e804215105f5beebd6614c4
syed <umaranwar707@gmail.com>
2020-10-09 09:48:28 +0530

    Added:emp present or absent

--- emp.sh
+++ emp.sh
@@ -1,1 +1,13 @@
-This is my employee wage program
+#!/bin/bash -x
+
+isfulltime=1
+chech=$((RANDOM%2))
+if [ $chech -eq $isfulltime ]
+then
+        rateperhour=20
+        hrs=8
+        salary=$(($rateperhour*$hrs))
+else
+        salary=0
+fi
+echo "your one day salary is" $salary
